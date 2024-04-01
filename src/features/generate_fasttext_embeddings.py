756d330d27ce85d3653c96dadded76229171f375
semcovici <pedrosemcovici@outlook.com>
2024-04-01 01:18:26 -0300

    add generate fasttext embeddings for separated comments

--- src/features/generate_fasttext_embeddings.py
+++ src/features/generate_fasttext_embeddings.py
@@ -1,5 +1,89 @@
-import fasttext.util
+##########################
+# Imports 
+##########################
+import fasttext
+from huggingface_hub import hf_hub_download
+import pandas as pd
+from nltk.tokenize import word_tokenize
+import numpy as np
+from tqdm import tqdm 
+import os
+tqdm.pandas()
 
-fasttext.util.download_model('pt', if_exists='ignore')
- 
-ft = fasttext.load_model('cc.en.300.bin')
+##########################
+# Aux Functions 
+##########################
+def get_mean_embeddings(text, model):
+        
+    try: 
+        # tokenize
+        tokens = word_tokenize(text, language='portuguese')
+        
+        # get mean embeddings 
+        mean_embeddings = np.array([model[str(tk)] for tk in tokens]).mean(axis = 0)
+    except Exception as e:
+         
+         print('-------------- WARNING --------------')
+         print('An exception occurred')
+         print(e)
+         
+         print('The text input is', text)
+         print('-------------- WARNING --------------')
+         
+         print(text)
+         
+         
+         
+         mean_embeddings = None
+    
+    
+    return mean_embeddings
+
+
+##########################
+# Definitions
+##########################
+model_name = 'facebook/fasttext-pt-vectors'
+
+path_processed_data = 'data/processed/'
+
+check_if_file_exists = True
+
+
+##########################
+# Process
+##########################
+def main():
+    
+    # load model
+    model_path = hf_hub_download(repo_id="facebook/fasttext-pt-vectors", filename="model.bin")
+    model = fasttext.load_model(model_path)
+    
+    list_corpus = ['ig','bo', 'cl', 'co', 'gl', 'lu']
+    
+    for i, corpus in enumerate(list_corpus):
+        
+        print(f'###### START {corpus} ({i + 1} of {len(list_corpus)}) ######')
+        
+        path_output = path_processed_data + f'train_r3_{corpus}_separated_comments_{model_name.replace('/', '_')}.parquet'
+        if os.path.isfile(path_output) and check_if_file_exists: 
+            
+            print('The file exists')
+            print("File name: ",path_output)
+            
+        else:
+            
+            # get data
+            data = pd.read_csv(path_processed_data + f'train_r3_{corpus}_separated_comments.csv')
+
+            # create column with embeddings
+            data['mean_embeddings'] = data.Texts.progress_apply(lambda x: get_mean_embeddings(x, model))
+            
+            # save dataset
+            data.to_parquet(path_output,index = False)
+            
+        print(f'###### END {corpus} ({i + 1} of {len(list_corpus)}) ######')
+
+
+if __name__ == '__main__':
+    main()
