1a3896facac996556d0b273ebda18bc21f5d36f2
semcovici <pedrosemcovici@outlook.com>
2024-04-22 01:42:46 -0300

    add generate results script

--- src/models/classification_methods.py
+++ src/models/classification_methods.py
@@ -11,10 +11,12 @@ from nltk.corpus import stopwords
 from tqdm import tqdm
 import nltk
 from imblearn.pipeline import Pipeline as IMBPipeline
-
+import gc
 from sklearn.compose import ColumnTransformer
 from datetime import datetime
 from sklearn.dummy import DummyClassifier
+from joblib_progress import joblib_progress
+from joblib import Parallel, delayed
 from sklearn.feature_selection import SelectPercentile
 
 
@@ -26,10 +28,55 @@ def get_classification_report(y_test, y_pred):
     return df_classification_report
 
 
+# Leitura de dados
+def read_pandas(path, file_type="csv", read_data_args={}):
+    if file_type == "csv":
+        return pd.read_csv(path, **read_data_args)
+    elif file_type == "parquet":
+        return pd.read_parquet(path, **read_data_args)
+
+# Criar tuplas de treinamento e teste
+def create_train_test_tuples(list_train_paths, list_test_paths, target_list, n_jobs=-1, file_type="csv", read_data_args={}):
+    if len(list_train_paths) != len(list_test_paths) or len(list_train_paths) != len(target_list):
+        raise ValueError("As listas não têm o mesmo comprimento")
+
+    data_paths = zip(list_train_paths, list_test_paths, target_list)
+    func_read_data = lambda a, b, c: (
+        read_pandas(a, file_type, read_data_args),
+        read_pandas(b, file_type, read_data_args),
+        c,
+    )
+
+    with joblib_progress("Lendo dados...", total=len(list_train_paths)):
+        parallel = Parallel(n_jobs=n_jobs)
+        return parallel(delayed(func_read_data)(a, b, c) for a, b, c in data_paths)
+
+# Geração de resultados
+def generate_results(data_tuples_list, corpus_name, X_col, clf, reports_path, estimator_name=None):
+    if estimator_name is None:
+        estimator_name = clf["estimator"].__class__.__name__
+
+    df_cr, df_test_results = process_classification(
+        **clf,
+        data_tuples=data_tuples_list,
+        X_cols=X_col,
+    )
+
+    str_cols = "emb" if "emb" in X_col[0] else "_".join(X_col)
+
+    cr_path = f"{reports_path}classification_reports/{estimator_name}_{corpus_name}_{str_cols}_classification_report.csv"
+    test_results_path = f"{reports_path}test_results/{estimator_name}_{corpus_name}_{str_cols}_test_results.csv"
+
+    df_cr.to_csv(cr_path)
+    df_test_results.to_csv(test_results_path)
+
+    return df_cr, df_test_results
+
+
 def process_classification(
         estimator,
         data_tuples, 
-        vectorizer = None,
+        preprocessing = None,
         sampling = None, 
         selection = None,
         scaling = None,
@@ -41,11 +88,11 @@ def process_classification(
     df_test_results = pd.DataFrame({})
 
     for data_train, data_test, target in data_tuples:
-
-        X_train = data_train[X_cols]
+        
+        X_train = data_train[X_cols].squeeze()
         y_train = data_train[y_col]
         
-        X_test = data_test[X_cols]
+        X_test = data_test[X_cols].squeeze()
         y_test = data_test[y_col]
         
         le = LabelEncoder()
@@ -55,20 +102,18 @@ def process_classification(
         y_test_enc = le_trained.transform(y_test)
 
         steps = [
-            ('vectorizer', vectorizer),
+            ('preprocessing', preprocessing),
             ('sampling', sampling),
             ('scaling', scaling),
             ('selection', selection),
             ('estimator',estimator)
         ]
+        
+        pipe = IMBPipeline(steps,verbose = False)
 
-        pipe = IMBPipeline(steps,verbose = True)
-
-        print('Training ...')
-        pipe_trained = pipe.fit(X_train, y_train_enc)
-
-        y_pred = pipe_trained.predict(X_test)
-        y_pred_proba = pipe_trained.predict_proba(X_test)
+        pipe.fit(X_train, y_train_enc)
+        y_pred = pipe.predict(X_test)
+        y_pred_proba = pipe.predict_proba(X_test)
         
         df_classification_report = get_classification_report(y_test_enc, y_pred)
         
@@ -85,5 +130,8 @@ def process_classification(
                 'pred_proba': [list(y_pred_proba)]    
             })
             ])
+        
+        del X_train, y_train, X_test, y_test, y_train_enc, y_test_enc        
+        gc.collect()
         
     return df_cr, df_test_results
---
+++ src/models/generate_results.py
@@ -0,0 +1,167 @@
+import pandas as pd
+from tqdm import tqdm
+from sklearn.dummy import DummyClassifier
+from joblib import Parallel, delayed
+from joblib_progress import joblib_progress
+from datetime import datetime
+import pandas as pd
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
+import xgboost as xgb
+from xgboost import XGBClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import LabelEncoder, MaxAbsScaler
+import numpy as np
+from imblearn.over_sampling import RandomOverSampler, SMOTE
+from imblearn.pipeline import Pipeline as IMBPipeline
+from sklearn.feature_selection import RFE
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.ensemble import RandomForestClassifier
+from nltk.corpus import stopwords
+from tqdm import tqdm
+tqdm.pandas()
+from sklearn.compose import ColumnTransformer
+from sklearn.feature_selection import RFE
+from tsfresh.feature_extraction import MinimalFCParameters
+from tsfresh.feature_extraction import extract_features
+tqdm.pandas()
+import warnings
+warnings.filterwarnings('ignore') 
+
+
+# Aviso para ignorar alertas desnecessários
+warnings.filterwarnings("ignore")
+
+import sys
+sys.path.append('src/')
+from models.classification_methods import create_train_test_tuples, generate_results 
+
+model_name = 'neuralmind/bert-base-portuguese-cased'
+
+random_seed = 42
+
+raw_data_path = 'data/raw/'
+processed_data_path = 'data/processed/'
+results_cr_path = 'reports/classification_reports/'
+test_results_path = 'reports/test_results/'
+reports_path = 'reports/'
+
+target_list = ['ig','bo', 'cl', 'co', 'gl', 'lu']
+
+top_ment_time_path = raw_data_path + '{}_r3_{}_top_mentioned_timelines.csv'
+list_train_paths_tmt = [top_ment_time_path.format("train",t) for t in target_list]
+list_test_paths_tmt = [top_ment_time_path.format("test",t) for t in target_list]
+
+users_path = raw_data_path + 'r3_{}_{}_users.csv'
+list_train_paths_users = [users_path.format(t,"train") for t in target_list]
+list_test_paths_users = [users_path.format(t,"test") for t in target_list]
+
+model_name = 'neuralmind/bert-base-portuguese-cased'
+
+top_ment_time_emb_path = processed_data_path + '{}_r3_{}_top_mentioned_timelines_{}.parquet'
+list_train_paths_tmt_emb = [top_ment_time_emb_path.format("train",t, model_name.replace("/", "_")) for t in target_list]
+list_test_paths_tmt_emb = [top_ment_time_emb_path.format("test",t, model_name.replace("/", "_")) for t in target_list]
+
+
+users_emb_path = processed_data_path + 'r3_{}_{}_users_{}.parquet'
+list_train_paths_users_emb = [users_emb_path.format(t,"train", model_name.replace("/", "_")) for t in target_list]
+list_test_paths_users_emb = [users_emb_path.format(t,"test", model_name.replace("/", "_")) for t in target_list]
+
+
+
+clf_to_test = {
+    'dummy': {
+        'estimator': DummyClassifier()
+    },
+    'tfidf_xgb':{
+        'preprocessing': TfidfVectorizer(
+                    stop_words = stopwords.words('portuguese'),
+                    lowercase = True,
+                    # ngram_range = (1,3),
+                    # max_features=50
+                    
+                    ),
+        'scaling': MaxAbsScaler(),
+        'estimator':  XGBClassifier(
+                random_state = 42,
+                verbosity = 3,
+                # device = 'cuda',
+                # tree_method = 'hist'
+                )
+    }
+}
+
+clf_to_test_emb = {
+    'bertimbau_xgb':{
+        'scaling': MaxAbsScaler(),
+        'estimator':  XGBClassifier(
+                random_state = 42,
+                verbosity = 3,
+                # device = 'cuda',
+                # tree_method = 'hist'
+                )
+    }
+}
+
+
+# X_cols_comb: possible combinations of X_col
+config_experiments_dict = {
+    'top_mentioned_timelines':{
+        'list_train_paths': list_train_paths_tmt,
+        'list_test_paths' : list_test_paths_tmt,
+        'file_type': 'csv',
+        'read_data_args' : {'sep': ';', 'encoding': 'utf-8-sig'},
+        'X_cols_comb': [['Texts']],
+        'clf_to_test': clf_to_test
+    },
+    'users':{
+        'list_train_paths': list_train_paths_users,
+        'list_test_paths' : list_test_paths_users,
+        'file_type': 'csv',
+        'read_data_args' : {'sep': ';', 'encoding': 'utf-8-sig'},
+        'X_cols_comb': [['Timeline'], ['Stance']],
+        'clf_to_test': clf_to_test
+    },
+    'users_emb':{
+        'list_train_paths': list_train_paths_users_emb,
+        'list_test_paths' : list_test_paths_users_emb,
+        'file_type': 'parquet',
+        'read_data_args' : {},
+        'X_cols_comb': [
+            [f'Timeline_emb_{i + 1}' for i in range(768)], 
+            [f'Stance_emb_{i + 1}' for i in range(768)]
+            ],
+        'clf_to_test': clf_to_test_emb
+    },
+    'top_mentioned_timelines_emb':{
+        'list_train_paths': list_train_paths_tmt_emb,
+        'list_test_paths' : list_test_paths_tmt_emb,
+        'file_type': 'parquet',
+        'read_data_args' : {},
+        'X_cols_comb': [
+            [f'Texts_emb_{i + 1}' for i in range(768)]
+            ],
+        'clf_to_test': clf_to_test_emb
+    }
+}
+
+# Execução dos experimentos
+for corpus, config in config_experiments_dict.items():
+    data_tuples_list = create_train_test_tuples(
+        list_train_paths=config["list_train_paths"],
+        list_test_paths=config["list_test_paths"],
+        target_list=target_list,
+        file_type=config["file_type"],
+        read_data_args=config["read_data_args"],
+    )
+
+    for X_col in config["X_cols_comb"]:
+        for clf_name, clf in config["clf_to_test"].items():
+            generate_results(
+                data_tuples_list,
+                corpus,
+                X_col,
+                clf,
+                reports_path,
+                estimator_name=clf_name,
+            )
