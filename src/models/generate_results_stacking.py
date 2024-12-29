66427261942c42f286a187e2c3f2544bdbfd7f4e
semcovici <pedrosemcovici@outlook.com>
2024-12-29 16:06:14 -0300

    add stacking with BoF BoFr BoM

--- src/models/generate_results_stacking.py
+++ src/models/generate_results_stacking.py
@@ -10,6 +10,10 @@ from tqdm import tqdm
 import sys
 sys.path.append('src/')
 from models.classification_methods import create_test_results_df    
+import warnings
+import itertools
+# Configurações globais
+warnings.filterwarnings('ignore')
 
 # Constants for paths
 PROCESSED_DATA_PATH = 'data/processed/'
@@ -19,6 +23,12 @@ TRAIN_RESULTS_PATH = 'reports/train_results/'
 PATH_BEST_UFT = "XGBClassifier_TfidfVectorizer_{target}_top_mentioned_timelines_Texts_{split}_results.csv"
 PATH_BEST_UT = "XGBClassifier_TfidfVectorizer_{target}_users_Timeline_{split}_results.csv"
 PATH_BEST_S = "bert_classifier_pablocosta_bertabaporu_base_uncased_{target}_Stance_{split}_results.csv"
+PATH_BEST_BoM = "TfidfVectorizer_SelectKBest_LogisticRegression_{target}_BagOfMentions_{split}_results.csv"
+PATH_BEST_BoF = "TfidfVectorizer_SelectKBest_LogisticRegression_{target}_BagOfFollowers_{split}_results.csv"
+PATH_BEST_BoFr = "TfidfVectorizer_SelectKBest_LogisticRegression_{target}_BagOfFriends_{split}_results.csv"
+
+
+PATH_STATEMENTS = PROCESSED_DATA_PATH + "r3_{target}_{split}_statements.csv"
 PATH_USERS = PROCESSED_DATA_PATH + "r3_{target}_{split}_users_processed.csv"
 PATH_TMT = PROCESSED_DATA_PATH + "{split}_r3_{target}_top_mentioned_timelines_processed.csv"
 
@@ -117,21 +127,75 @@ def preprocess_data(target):
         text_col = "S", 
         original_df_path = PATH_USERS.format(split="test", target=target)
         )
+    # NOVO: leitura do BagOfMentions (train/test)
+    train_BoM = read_data(
+        results_df_path = TRAIN_RESULTS_PATH + PATH_BEST_BoM.format(split='train', target=target),
+        text_col = "BoM",
+        original_df_path = PATH_STATEMENTS.format(split="train", target=target)
+    )
+    test_BoM = read_data(
+        results_df_path = TEST_RESULTS_PATH + PATH_BEST_BoM.format(split='test', target=target),
+        text_col = "BoM",
+        original_df_path = PATH_STATEMENTS.format(split="test", target=target)
+    )
+
+    # NOVO: leitura do BagOfFollowers (train/test)
+    train_BoF = read_data(
+        results_df_path = TRAIN_RESULTS_PATH + PATH_BEST_BoF.format(split='train', target=target),
+        text_col = "BoF",
+        original_df_path = PATH_STATEMENTS.format(split="train", target=target)
+    )
+    test_BoF = read_data(
+        results_df_path = TEST_RESULTS_PATH + PATH_BEST_BoF.format(split='test', target=target),
+        text_col = "BoF",
+        original_df_path = PATH_STATEMENTS.format(split='test', target=target)
+    )
+
+    # NOVO: leitura do BagOfFriends (train/test)
+    train_BoFr = read_data(
+        results_df_path = TRAIN_RESULTS_PATH + PATH_BEST_BoFr.format(split='train', target=target),
+        text_col = "BoFr",
+        original_df_path = PATH_STATEMENTS.format(split="train", target=target)
+    )
+    test_BoFr = read_data(
+        results_df_path = TEST_RESULTS_PATH + PATH_BEST_BoFr.format(split='test', target=target),
+        text_col = "BoFr",
+        original_df_path = PATH_STATEMENTS.format(split="test", target=target)
+    )
     
     # fill null values with -1
     filled_train_UFT = fill_missing_indices(train_UFT).fillna(-1)
     filled_train_UT = fill_missing_indices(train_UT).fillna(-1)
     filled_train_S = fill_missing_indices(train_S).fillna(-1)
     
-    train = pd.concat([filled_train_UFT, filled_train_UT, filled_train_S], axis = 1)
-    test = pd.concat([test_UFT, test_UT, test_S], axis = 1)
-
+    # fill missing indices (there is no missing indices) 
+    filled_train_BoM = train_BoM.copy().reset_index(drop=True)
+    filled_train_BoF = train_BoF.copy().reset_index(drop=True)
+    filled_train_BoFr= train_BoFr.copy().reset_index(drop=True)
+    
+    train = pd.concat([
+        filled_train_UFT, 
+        filled_train_UT, 
+        filled_train_S,
+        filled_train_BoM,
+        filled_train_BoF, 
+        filled_train_BoFr
+        ], axis = 1)
+    test = pd.concat([
+        test_UFT, 
+        test_UT, 
+        test_S,
+        test_BoM.reset_index(drop=True),
+        test_BoF.reset_index(drop=True),
+        test_BoFr.reset_index(drop=True)
+        ], axis = 1)
+ 
     if train.isna().sum().sum() > 0:
         raise TypeError("Null data in train")
     if test.isna().sum().sum() > 0:
         raise TypeError("Null data in test")
 
-
+    # FALTA ADICIONAR AS VERIFICACOES PARA OS Bag of ...
     if len(train[~ (train.test_UFT == train.test_UT) & ((train.test_UT !=-1) & (train.test_UFT !=-1))]) > 0: 
         raise ValueError("há valores inconsistentes para a label")
 
@@ -147,8 +211,14 @@ def preprocess_data(target):
     y_train = [train_label_UFT[i] if train_label_UFT[i] != -1 else train_label_UT[i]  for i in range(len(train_label_UFT))]
     y_test = [test_label_UFT[i] if test_label_UFT[i] != -1 else test_label_UT[i]  for i in range(len(test_label_UFT))]
 
-
-    cols_to_drop = ['test_UFT',"pred_UFT",'test_UT', 'pred_UT', "pred_proba_0_UFT", "pred_proba_0_UT", "pred_proba_0_S", 'pred_S', 'test_S']
+    cols_to_drop = [
+        
+        "pred_UFT", 'pred_UT', 'pred_S', "pred_BoM", 'pred_BoF', 'pred_BoFr',
+        
+        'test_UFT', 'test_UT', 'test_S', "test_BoM", 'test_BoF', 'test_BoFr',
+        
+        "pred_proba_0_UFT", "pred_proba_0_UT", "pred_proba_0_S", "pred_proba_0_BoM", 'pred_proba_0_BoF', 'pred_proba_0_BoFr'
+        ]
     
     X_train_full = train.drop(cols_to_drop,axis = 1)
     X_test_full = test.drop(cols_to_drop,axis = 1)
@@ -190,17 +260,22 @@ def perform_model_search(X_train, y_train, X_test, y_test, comb, results_path, t
     df_test_results.to_csv(results_path, index=False)
 
 
+my_list = [
+    'Texts', 'Timeline', 'Texts',
+     'BoM', "BoF", "BoFr"
+    ]
+
+all_combinations = []
+for r in range(2, len(my_list) + 1):
+    all_combinations.extend(list(itertools.combinations(my_list, r)))
+
+print('All combinations: \n', all_combinations)
+
 for target in tqdm(target_list):
     
     X_train_full, X_test_full, y_train, y_test = preprocess_data(target)
-
-
-    for comb in [
-        ('Texts', 'Timeline'),
-        ('Texts', 'Stance'),
-        ('Stance', 'Timeline'),
-        ('Stance', 'Timeline', 'Texts')
-    ]:
+    
+    for comb in all_combinations:
 
         str_cols = "_".join(comb)
 
@@ -216,7 +291,18 @@ for target in tqdm(target_list):
         if "Stance" not in comb:
             X_train.drop([col for col in X_train.columns if "S" in col], axis=1, inplace=True)
             X_test.drop([col for col in X_test.columns if "S" in col], axis=1, inplace=True)
+        if "BoM" not in comb:
+            X_train.drop([col for col in X_train.columns if "BoM" in col], axis=1, inplace=True)
+            X_test.drop([col for col in X_test.columns if "BoM" in col], axis=1, inplace=True)
+        if "BoF" not in comb:
+            X_train.drop([col for col in X_train.columns if "BoF" in col], axis=1, inplace=True)
+            X_test.drop([col for col in X_test.columns if "BoF" in col], axis=1, inplace=True)
+        if "BoFr" not in comb:
+            X_train.drop([col for col in X_train.columns if "BoFr" in col], axis=1, inplace=True)
+            X_test.drop([col for col in X_test.columns if "BoFr" in col], axis=1, inplace=True)    
+        
 
         results_path = f"{TEST_RESULTS_PATH}/Ensemble_LogisticRegression_{target}_{str_cols}_test_results.csv"
         perform_model_search(X_train, y_train, X_test, y_test, comb, results_path, target)
         print("Results saved at:", results_path)
+        
